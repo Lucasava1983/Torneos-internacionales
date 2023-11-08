390b1b23a378bdad5013c37b278df8ae494cd93f
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-07 21:36:25 -0300

    templates DIR

--- international_cup/settings.py
+++ international_cup/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'international_cup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/"templates_base",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
