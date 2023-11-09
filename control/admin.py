5c76be4630eb89bde018cdc1996f5aa1e14692e2
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-09 18:17:21 -0300

    panel administrativo

--- control/admin.py
+++ control/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from control.models import Copa_Libertadores, Copa_Sudamericana, Champions_League
+
+admin.site.register(Copa_Libertadores)
+admin.site.register(Copa_Sudamericana)
+admin.site.register(Champions_League)
