5408deb78c33c062b1830fb3de80a74d5b88c0c1
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-09 03:58:53 -0300

    1111

--- international_cup/urls.py
+++ international_cup/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from international_cup.views import teams_html
 from control.views import teams
 
 urlpatterns = [
--- international_cup/views.py
+++ international_cup/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def teams(request):
+def teams_html(request):
     contexto = {
         "local" : "Fluminense",
         "visitante" : "Boca Juniors"
