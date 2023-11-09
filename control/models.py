b74d1c39e3fa522d4e711e8f840448bd6a2f7d14
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-09 18:36:27 -0300

    str

--- control/models.py
+++ control/models.py
@@ -8,6 +8,9 @@ class Copa_Libertadores(models.Model):
     runner_up= models.CharField(max_length=250)
     best_player= models.CharField(max_length=250, blank=True)
 
+    def __str__(self):
+        return f'{self.champion}, {self.runner_up}'
+
 class Copa_Sudamericana(models.Model):
     continent= models.CharField(max_length=100)
     season=models.IntegerField()
@@ -15,9 +18,15 @@ class Copa_Sudamericana(models.Model):
     runner_up= models.CharField(max_length=250)
     best_player= models.CharField(max_length=250, blank=True)
 
+    def __str__(self):
+        return f'{self.champion}, {self.runner_up}'
+
 class Champions_League(models.Model):
     continent= models.CharField(max_length=100)
     season=models.IntegerField()
     champion= models.CharField(max_length=250)
     runner_up= models.CharField(max_length=250)
     best_player= models.CharField(max_length=250, blank=True)
+
+    def __str__(self): 
+        return f'{self.champion}, {self.runner_up}'
