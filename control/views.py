9a9a3e92a7a761788511ca4999141802a8aa87f6
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-10 15:23:57 -0300

    sudamericana

--- control/views.py
+++ control/views.py
@@ -15,3 +15,29 @@ def teams(request):
     )
 
     return http_response
+
+def teams(request):
+    contexto = {
+        "visitante" : Champions_League.objects.all(),
+    }
+
+    http_response = render(
+        request=request,
+        template_name='control/equipos.html',
+        context=contexto,
+    )
+
+    return http_response
+
+def teams(request):
+    contexto = {
+        "visitante" : Copa_Sudamericana.objects.all(),
+    }
+
+    http_response = render(
+        request=request,
+        template_name='control/equipossud.html',
+        context=contexto,
+    )
+
+    return http_response
