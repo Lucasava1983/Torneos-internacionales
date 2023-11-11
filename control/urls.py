885df708dd76e72a261a75653690c24a0131d7b1
Carlos Sánchez <lucasava.2109@gmail.com>
2023-11-11 18:22:46 -0300

    act

--- control/urls.py
+++ control/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from control.views import teams, league, sudamericana, equipos_clasificados, buscar_equipos
 
 urlpatterns = [
-    path('teams-squad/', teams, name="equipos_libertadores"),
+    path('equipos_libertadores/', teams, name="equipos_libertadores"),
     path('equipo-CL/', league, name="equipos_champions"),
     path('equipos_sudamericana', sudamericana, name='equipos_sudamericana'),
-    path('cupos/', equipos_clasificados, name='lista'),
+    path('lista/', equipos_clasificados, name='lista'),
     path('buscar_equipos/', buscar_equipos, name='buscar_equipos',)
 ]
 
